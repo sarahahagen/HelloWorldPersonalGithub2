9e48ee29a6e4995451ed42ed7823ba740931245b
sarahahagen <shagen@zagmail.gonzaga.edu>
2021-09-02 11:55:18 -0700

    added to the string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println("Hello CPSC 312");
+        System.out.println("Hello CPSC 312!!! :)");
     }
 }
